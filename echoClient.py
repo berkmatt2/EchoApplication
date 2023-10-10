a37038997633fa6e6350bc3e8d743c6cb91c166c
berkmatt2 <112998569+berkmatt2@users.noreply.github.com>
2023-10-10 13:37:35 -0400

    Add files via upload

--- echoClient.py
+++ echoClient.py
@@ -4,55 +4,47 @@ import argparse
 import time
 
 def echo_client(port, host):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (host, port)
-    sock.connect(server_address)
-    message = input("<PROTOCOL PHASE><WS><MEASUREMENT TYPE><WS><NUMBER OF PROBES><WS><MESSAGE SIZE><WS><SERVER DELAY>\n")    
-    sock.sendto(message.encode(), server_address)
-    newMessage, server_address2 = sock.recvfrom(2048)
-    print(newMessage.decode())
-    if newMessage.decode() == "200 OK: Ready":
-        splitMsg = message.split()
-        measureType = splitMsg[1]
-        numProbes = int(splitMsg[2])
-        msgSize = int(splitMsg[3])
-        temp = splitMsg[4]
-        delay = temp[:-2]
-        if measureType == "rtt":
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (host, port)
+        sock.connect(server_address)
+        print("<PROTOCOL PHASE><WS><MEASUREMENT TYPE><WS><NUMBER OF PROBES><WS><MESSAGE SIZE><WS><SERVER DELAY>\n")
+        print("or <PROTOCOL PHASE>\n to quit")
+        message = input("Enter a command: ")
+        sock.sendto(message.encode(), server_address)
+        newMessage, server_address2 = sock.recvfrom(2048)
+        print(newMessage.decode())
+        if newMessage.decode() == "200 OK: Closing Connection":
+            sys.exit(0)
+        elif newMessage.decode() == "200 OK: Ready":
+            splitMsg = message.split()
+            measureType = splitMsg[1]
+            numProbes = int(splitMsg[2])
+            msgSize = int(splitMsg[3])
+            temp = splitMsg[4]
+            delay = temp[:-2]
             totalTime = 0
             for i in (range(numProbes)):
                 result = rtt(numProbes, msgSize, i + 1, sock, server_address)
                 if result == -500:
                     print("404 ERROR: Invalid Measurement Message")
                     break
-            totalTime += result
+                totalTime += result
             roundTripTime = totalTime / numProbes
-            print("Average RTT for ", numProbes, " Messages of Size ", msgSize, "With Server Delay ", delay, ": ", roundTripTime)
-        if measureType == "tput":
-            totalTime = 0
-            for i in (range(numProbes)):
-                result = rtt(numProbes, msgSize, i + 1, sock, server_address)
-                if result == -500:
-                    print("404 ERROR: Invalid Measurement Message")
-                    break
-                else:
-                    totalTime += result
-            roundTripTime = totalTime / numProbes
-            throughput = (msgSize * 8 * 0.001) / roundTripTime
-            print("Throughput for ", numProbes, " Messages of Size ", msgSize, "With Server Delay ", delay, ": ", throughput)
-            
-
-            
-        
+            if measureType == "rtt":
+                print("Average RTT for ", numProbes, " Messages of Size ", msgSize, "With Server Delay ", delay, ": ", roundTripTime)
+            if measureType == "tput":
+                throughput = (msgSize * 8 * 0.001) / roundTripTime
+                print("Throughput for ", numProbes, " Messages of Size ", msgSize, "With Server Delay ", delay, ": ", throughput)
+      
 def rtt(numProbes, payloadSize, probeID, socket, server_address):
     protocolPhase = "m"
     payload = 's' * payloadSize
     probeIDString = str(probeID)
     message = str(protocolPhase + " " + probeIDString + " " + payload + "\n")
-    print(message)
     startTime = time.time()
     socket.sendto(message.encode(), server_address)
-    response, server_address2 = socket.recvfrom(2048)
+    response, server_address2 = socket.recvfrom(35000)
     if response.decode() == "404 ERROR: Invalid Measurement Message":
         return -500
     endTime = time.time()
--- echoServer.py
+++ echoServer.py
@@ -17,8 +17,56 @@ def echo_server(port):
         data = client.recv(data_payload)
         if data:
             message = data.decode()
-            splitMsg = message.split()
+            if " " in message:
+                splitMsg = message.split()
+            else:
+                splitMsg = message
+            print(splitMsg)
+            if splitMsg[0] == "t":
+                if splitMsg[:-2] == "t":
+                    response = "200 OK: Closing Connection"
+                    client.send(response.encode())
+                else:
+                    response = "404 ERROR: Invalid Connection Termination Message"
+                    client.send(response.encode())
+                client.close()
+                sys.exit(0)
             isValid = True
+            try:
+                splitMsg[0]
+            except IndexError:
+                response = "404 ERROR: Invalid Connection Setup Message"
+                client.send(response.encode())
+                client.close()
+                sys.exit(0)
+            try:
+                splitMsg[1]
+            except IndexError:
+                response = "404 ERROR: Invalid Connection Setup Message"
+                client.send(response.encode())
+                client.close()
+                sys.exit(0)
+            try:
+                splitMsg[2]
+            except IndexError:
+                response = "404 ERROR: Invalid Connection Setup Message"
+                client.send(response.encode())
+                client.close()
+                sys.exit(0)
+            try:
+                splitMsg[3]
+            except IndexError:
+                response = "404 ERROR: Invalid Connection Setup Message"
+                client.send(response.encode())
+                client.close()
+                sys.exit(0)
+            try:
+                splitMsg[4]
+            except IndexError:
+                response = "404 ERROR: Invalid Connection Setup Message"
+                client.send(response.encode())
+                client.close()
+                sys.exit(0)
             if splitMsg[0] != "s":
                 isValid = False
             if splitMsg[1] != "rtt" and splitMsg[1] != "tput":
@@ -49,24 +97,24 @@ def echo_server(port):
                 probes = numProbes
                 size = numBytes
                 delay = servDelay
-                rtt(probes, delay, sock, server_address)
+                rtt(probes, delay, sock, server_address, client)
             else:
                 response = "404 ERROR: Invalid Connection Setup Message"
                 client.send(response.encode())
                 client.close()
                 
-def rtt(numProbes, serverDelay, sock, server_address):
+def rtt(numProbes, serverDelay, sock, server_address, client):
     probeNumber = 0
     delay = serverDelay / 1000
-    client, address = sock.accept()
     response = "200 OK: Ready"
     client.send(response.encode())
     while True:
-        data = client.recv(data_payload)
+        data = client.recv(35000)
         if data:
             message = data.decode()
             splitMsg = message.split()
-            newProbeNum = splitMsg[1]
+            newProbeNum = int(splitMsg[1])
+            print(newProbeNum)
             if newProbeNum == (probeNumber + 1) and newProbeNum <= numProbes:
                 probeNumber += 1
                 time.sleep(delay)
